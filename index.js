5fa6998dedefc4cc4eae486da86cf0d0139b7bfc
pitunti <pi121992@gmail.com>
2021-08-28 21:15:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const options={
+  cors:{
+    origin:"*" /*Permitir todas las conecciones si quieres que solo se permita un dominio en especifico por ejemplo https://localhost*/
+  }
+}
+const io = require("socket.io",options);//Ejecuta socket.io
+
+io.listen(1212)//Inicia el socket server. Solo se especifica el puerto para la coneccion
+
+
 
